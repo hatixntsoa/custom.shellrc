d22bbc63d6d71cba009f57cf323a7c78da4cb3ad
Hatix Ntsoa <hatixntsoa@gmail.com>
2025-04-25 10:21:45 +0300

    feat: brave gpt

--- wsl/zshrc[h471x].sh
+++ wsl/zshrc[h471x].sh
@@ -1670,7 +1670,7 @@ alias rtr='open_chrome_app "www.airtel.mg" "Airtel Router"'
 alias htx="open_chrome_app 192.168.8.1 HTX_AP"
 
 # this alias to open chatGpt app
-alias gpt="open_chrome_app chatgpt.com ChatGPT"
+alias gpt="open_brave_app chatgpt.com ChatGPT"
 
 # this alias to open LeChat
 alias lechat="open_brave_app chat.mistral.ai LeChat"

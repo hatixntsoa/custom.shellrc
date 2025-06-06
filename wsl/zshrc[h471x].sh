6848c418d93664c3e6e623004cf142135e8a337a
Hatix Ntsoa <hatixntsoa@gmail.com>
2025-06-06 13:23:35 +0300

    refactor: removed unused comments

--- wsl/zshrc[h471x].sh
+++ wsl/zshrc[h471x].sh
@@ -2663,16 +2663,6 @@ alias sshon="allow_sudo && sshon"
 
 # this function for sshon alias
 function sshon {
-  # local is_wsl=$(grep -qi microsoft /proc/version && echo true || echo false)
-  #
-  # if $is_wsl; then
-  #   sudo service ssh start &>/dev/null;
-  # else
-  #   svc_on ssh;
-  # fi
-
-  # UPDATED : 2025-05-30 09:33
-  # WSL now boot with systemd
   sudo systemctl start ssh
   sth;
 }
@@ -2682,13 +2672,6 @@ alias sshoff="allow_sudo && sshoff"
 
 # this function for sshoff alias
 function sshoff {
-  # # sudo /etc/init.d/ssh stop;
-  # if [[ $(pgrep sshd) ]]; then
-  #   sudo kill $(pgrep sshd);
-  # fi
-
-  # UPDATED : 2025-05-30 09:33
-  # WSL now boot with systemd
   sudo systemctl stop ssh
   sth;
 }

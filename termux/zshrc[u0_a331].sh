a182dda9b9deba18146b871816e9f60dc3b802f4
Hatix Ntsoa <hatixntsoa@proton.me>
2025-09-17 08:48:23 +0300

    feat: termux zshrc

---
+++ termux/zshrc[u0_a331].sh
@@ -0,0 +1,683 @@
+### Powerlevel10k Theme
+
+# Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.
+# Initialization code that may require console input (password prompts, [y/n]
+# confirmations, etc.) must go above this block; everything else may go below.
+if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]; then
+  source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
+fi
+
+# Make zsh instant prompt quiet
+# K_INSTANT_PROMPT=quiet
+
+# set the default editor
+export EDITOR=nvim
+
+#######################################################################
+
+### ZSH Basic Config
+
+set autocd         # change directory just by typing his name
+setopt promptsubst # enable command substitution in prompt
+
+# configure keybindings
+bindkey -e # emacs keybindings
+bindkey ' ' magic-space # do history expansion on space
+
+# Uncomment the following line to use case-sensitive completion.
+CASE_SENSITIVE="true"
+
+# termux path
+PATH=$PATH:/data/data/com.termux/files/home/go/bin
+
+#######################################################################
+
+### oh-my-zsh Config
+
+# Path to oh-my-zsh installation.
+export ZSH="$HOME/.oh-my-zsh"
+
+# Oh-My-Zsh custom theme
+ZSH_THEME="powerlevel10k/powerlevel10k"
+# ZSH_THEME="archcraft"
+
+# Which plugins would you like to load?
+# Standard plugins can be found in $ZSH/plugins/
+# Custom plugins may be added to $ZSH_CUSTOM/plugins/
+plugins=(
+  zsh-syntax-highlighting
+  zsh-autosuggestions
+  web-search
+  vscode
+  sudo
+)
+
+# Here to disable oh-my-zsh auto update
+DISABLE_AUTO_UPDATE="true"
+
+# This is for fastfile Oh-My-Zsh Plugin
+fastfile_var_prefix='$'
+
+# Declare the variable for zsh-syntax-highlighting styles
+typeset -A ZSH_HIGHLIGHT_STYLES
+
+# From ~/.oh-my-zsh/plugins/zsh-syntax-highlighting/highlighters/main/README.md
+ZSH_HIGHLIGHT_STYLES[path]='none'
+ZSH_HIGHLIGHT_STYLES[autodirectory]=fg='#ffffff'
+ZSH_HIGHLIGHT_STYLES[suffix-alias]=fg='none'
+ZSH_HIGHLIGHT_STYLES[precommand]=fg='none'
+ZSH_HIGHLIGHT_STYLES[arg0]=fg='none'
+ZSH_HIGHLIGHT_STYLES[single-quoted-argument]=fg='#23ff45'
+ZSH_HIGHLIGHT_STYLES[double-quoted-argument]=fg='#23ff45'
+ZSH_HIGHLIGHT_STYLES[single-hyphen-option]='fg=#37b0ff,bold'
+ZSH_HIGHLIGHT_STYLES[double-hyphen-option]='fg=#37b0ff,bold'
+ZSH_HIGHLIGHT_STYLES[redirection]='fg=#ffe541,bold'
+ZSH_HIGHLIGHT_STYLES[globbing]='fg=#ff9000,bold'
+ZSH_HIGHLIGHT_STYLES[command-substitution-unquoted]='fg=#d30ccf,bold'
+ZSH_HIGHLIGHT_STYLES[command-substitution-delimiter]='fg=#d30ccf,bold'
+
+# here the color of the auto-suggestion
+# ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE=fg=#0000ff"
+
+# source oh-my-zsh after everything have been loaded
+source $ZSH/oh-my-zsh.sh
+
+#######################################################################
+
+### ANSI CODES VARIABLES
+
+# Text Color
+BLACK="\e[30m"
+RED="\e[31m"
+GREEN="\e[32m"
+YELLOW="\e[33m"
+BLUE="\e[34m"
+MAGENTA="\e[35m"
+CYAN="\e[36m"
+LIGHT_GRAY="\e[37m"
+DARK_GRAY="\e[90m"
+LIGHT_RED="\e[91m"
+LIGHT_GREEN="\e[92m"
+LIGHT_YELLOW="\e[93m"
+LIGHT_BLUE="\e[94m"
+LIGHT_MAGENTA="\e[95m"
+LIGHT_CYAN="\e[96m"
+WHITE="\e[97m"
+
+# Background Color
+BG_BLACK="\e[40m"
+BG_RED="\e[41m"
+BG_GREEN="\e[42m"
+BG_YELLOW="\e[43m"
+BG_BLUE="\e[44m"
+BG_MAGENTA="\e[45m"
+BG_CYAN="\e[46m"
+BG_LIGHT_GRAY="\e[47m"
+BG_DARK_GRAY="\e[100m"
+BG_LIGHT_RED="\e[101m"
+BG_LIGHT_GREEN="\e[102m"
+BG_LIGHT_YELLOW="\e[103m"
+BG_LIGHT_BLUE="\e[104m"
+BG_LIGHT_MAGENTA="\e[105m"
+BG_LIGHT_CYAN="\e[106m"
+BG_WHITE="\e[107m"
+
+# Text Style
+BOLD="\e[1m"
+DIM="\e[2m"
+UNDERLINE="\e[4m"
+BLINK="\e[5m"
+INVERT="\e[7m"
+HIDDEN="\e[8m"
+
+# Reset
+RESET="\e[0m"
+RESETBG="\e[49m\n"
+
+# Character
+CHECKMARK="$(printf '\xE2\x9C\x94')"
+QUESTION_MARK="$(printf '\xE2\x9D\x93')"
+
+#######################################################################
+
+### Basic Aliases
+
+### Basic Aliases
+
+# this alias to update the package
+alias upd="allow_sudo && upd"
+
+# this function for up alias
+function upd {
+  echo "u p d a t i n g .  .  ." | figlet -t -c;
+  br && sudo apt update && br;
+}
+
+# this alia to update the package
+alias upg="allow_sudo && upg"
+
+# this function for up alias
+function upg {
+  echo "u p g r a d i n g . . ." | figlet -t -c;
+  br && sudo apt upgrade && br;
+}
+
+# this alias to install package
+alias ist="allow_sudo && ist"
+
+# this function for ist alias
+function ist {
+  case "${1##*.}" in
+    git)
+      echo "   C l o n i n g .  .  . " | figlet;
+      br && echo "Package =======> "${1%.*}" ";
+      br && git clone "$1" && br;
+      ;;
+    *)
+      echo "   I n s t a l l i n g .  .  . " | figlet;
+      c && br && echo "Package =======> "$1" ";
+      br && sudo apt install "$1" && br;
+      ;;
+  esac
+}
+
+# this alias to install package
+alias rmv="rmv"
+
+# this function for ist alias
+function rmv {
+  case "${1##*.}" in
+    deb)
+      c && echo -e && echo -e && sudo clear && echo -e && echo -e && echo "   r e m o v i n g .  .  . " | figlet | lolcat && echo -e && echo "Package =======> "${1%.*}" " && echo -e && sudo dpkg -r "$1" && echo -e;
+      ;;
+    *)
+      c && echo -e && echo -e && sudo clear && echo -e && echo -e && echo "   r e m o v  i n g .  .  . " | figlet | lolcat && echo -e && echo "Package =======> "$1" " && echo -e && sudo apt remove "$1" && echo -e;
+      ;;
+  esac
+}
+
+# this alias to rename a file / directory; and display it after
+alias nm="nm"
+
+# this function for cpf alias
+function nm {
+  mv "$1" "$2" && cv;
+}
+
+# this line to count line inside a file
+alias cl="linecount"
+
+# this function for the cl alias
+function linecount {
+  if [ -z "$1" ]; then
+    echo "Please provide a filename"
+  elif [ -z "$2" ]; then
+    wc -l "$1" | awk '{print $1, "lines"}'
+  else
+    grep -c "$1" "$2" | awk -v var="$1" '{print $1, var, "in it"}'
+  fi
+};
+
+# this line to view a command manual
+alias mn="mn"
+
+# this function for mn alias
+function mn {
+  if [[ $# -eq 1 ]]; then
+    man $1 | less
+  else
+    man $1 | grep $2 | less
+  fi
+}
+
+# this alias to view the history
+alias hst="hst"
+
+# this function for hst alias
+function hst {
+  if [[ $# -eq 0 ]]; then
+    history | less && cv
+  else
+    re='^[0-9]+$'
+    # check if the argument is an integrer
+    if [[ $1 =~ $re ]]; then
+      history | tail -$1 | less && cv
+    else  # else if it's a text to grep
+      history | grep "$1" | less && cv
+    fi
+  fi
+}
+
+# this alias to specify which command in the history to search
+alias hsg="hsg"
+
+# this function for hsg alias
+function hsg {
+  if [[ $# -eq 0 ]]; then
+    history | less && cv;
+  else
+    history | grep "$1" | less && cv;
+  fi
+}
+
+# this alias to count line and words inside a file
+alias flc="flc"
+
+# this function for the cl alias
+function flc {
+  if [[ -f "$1" ]]; then
+    if [ -z "$1" ]; then
+      echo "Please provide a filename"
+    elif [ -z "$2" ]; then
+      c && br;
+      echo -ne " ==> "
+      file "$1";
+      echo -ne " ==> ";
+      wc -l "$1" | awk '{print $1, "lines"}';
+      echo -ne " ==> ";
+      wc -w "$1" | awk '{print $1, "words"}';
+
+      # Display the file size using Bash arithmetic
+      file_size=$(stat -c%s "$1")
+      if (( file_size < 1024 )); then
+        echo " ==> Size : $file_size bytes"
+      elif (( file_size < 1048576 )); then
+        size_kb=$(awk "BEGIN {printf \"%.2f\", $file_size/1024}")
+        echo " ==> Size : $size_kb KB"
+      elif (( file_size < 1073741824 )); then
+        size_mb=$(awk "BEGIN {printf \"%.2f\", $file_size/1048576}")
+        echo " ==> Size : $size_mb MB"
+      else
+        size_gb=$(awk "BEGIN {printf \"%.2f\", $file_size/1073741824}")
+        echo " ==> Size : $size_gb GB"
+      fi
+
+      if command -v exiftool >/dev/null 2>&1; then
+        br
+        exiftool "$1"
+      fi
+      br;
+    else
+      grep -c "$1" "$2" | awk -v var="$1" '{print $1, var, "in it"}'
+    fi
+  elif [[ -d "$1" ]]; then
+    dc "$1";
+  fi
+}
+
+kill-line() {
+  if [[ $BUFFER == "" ]]; then
+    zle backward-kill-line
+  else
+    zle kill-whole-line
+  fi
+}
+
+zle -N kill-line
+bindkey "11" kill-line
+bindkey "²²" kill-line
+
+# this alias to clear
+alias c="clear"
+
+# this alias to clear but with extra lines
+alias x="clear && echo -e && echo -e && echo -e && echo -e && echo -e && echo -e"
+
+# Clear screen at startup
+# x
+
+# this alias to break a line
+alias br="br"
+
+# this function for br alias
+function br {
+  if [[ $# -eq 1 ]]; then
+    for ((i=1; i<=$1;i++)); do
+      echo -e;
+    done
+  elif [[ $# -eq 0 ]]; then
+    echo -e;
+  fi
+}
+
+# this alias to open a directory
+alias open="op"
+
+# this function for op alias
+function op {
+  # Check if $1 is a symbolic link
+  if [[ -L "$1" ]]; then
+    # Resolve the real path of the symbolic link
+    real_path=$(readlink -f "$1")
+    # Check if the resolved path is a directory
+    if [[ -d "$real_path" ]]; then
+      if [[ $# -eq 1 ]]; then
+        cd "$real_path" && cv
+      elif [[ $# -eq 2 ]]; then
+        cd "$real_path" && cvg "$2"
+      fi
+    # Check if the resolved path is a file
+    elif [[ -f "$real_path" ]]; then
+      vf "$real_path"
+    fi
+  # If $1 is a directory (but not a symbolic link)
+  elif [[ -d "$1" ]]; then
+    if [[ $# -eq 1 ]]; then
+      cd "$1" && cv
+    elif [[ $# -eq 2 ]]; then
+      cd "$1" && cvg "$2"
+    fi
+  # If $1 is a file (but not a symbolic link)
+  elif [[ -f "$1" ]]; then
+    vf "$1"
+  else
+    echo -ne "${BOLD}${GREEN}Create ${WHITE}new directory ${LIGHT_BLUE}$1 ${WHITE}? (y/n) ";
+    read check_dir
+    echo ${RESET}
+
+    if [ "$check_dir" = "y" ]; then
+      opdr "$1"
+    else
+      return 0
+    fi
+  fi
+}
+
+# this alias to force delete
+alias remove="rdf"
+
+# this function for rdf alias
+function rdf {
+  rm -rfv "$@";
+  cv;
+}
+
+# this alias to create a directory
+alias dr="dr"
+
+# this function for dr alias
+function dr {
+  mkdir "$@" && cv;
+}
+
+# this alias to remove a directory
+alias rd="rd"
+
+# this function for rd alias
+function rd {
+  rm -r "$@" && cv;
+}
+
+# this line to have destination location for copy / cut
+alias dt="dt"
+
+# this function for dst alias
+function dt {
+  dest="$PWD" && c && echo -e && echo "d e s t   s a v e d" | figlet -t -c && sleep 0.6 && cv;
+}
+
+# this alias to open a directory
+# and make it as destination
+alias opd="opd"
+
+# this function for opd alias
+function opd {
+  op "$1" && dt && nd;
+}
+
+# this alias to create a directory
+# and then directly enter to it
+alias opdr="opdr"
+
+# this function for opdr alias
+function opdr {
+  dr "$*" && op "$*";
+}
+
+# this alias to go back from a directory
+alias b="b"
+
+# this function for b alias
+function b {
+  if [[ $# -eq 0 ]]; then
+    cd .. && cv
+  else
+    for ((i=1; i<=$1;i++)); do
+      cd .. && cv
+    done
+  fi
+}
+
+# this alias to go to the previous directory
+alias nd="nd"
+
+# this function for nd alias
+function nd {
+  [[ -n "$dest" ]] && \
+    cd $dest || \
+    cd - &>/dev/null;
+  cv;
+}
+
+#######################################################################
+
+### Navigation Aliases
+
+# this alias to go to home directory
+alias hm="cd && cv"
+
+# this alias to edit $SHELLrc
+alias ct="nvim ~/.zshrc"
+
+# this alias to reload the zshrc file
+alias rld="rld"
+
+# this function for rld alias
+function rld {
+  c;
+
+  # local variables
+  local shellrc=.$(basename $SHELL)rc;
+  local platform="termux"
+  local backup_dir=$HOME/files/custom.shellrc/$platform;
+  local backup_file=zshrc[$USER].sh;
+  local saved_message="$shellrc backed up"
+
+  # source the config file and save it
+  source ~/$shellrc;
+  cat ~/$shellrc > $backup_dir/$backup_file;
+
+  # saved message display
+  c && br;
+  echo "${BOLD}${WHITE}$saved_message";
+  br && sleep 0.5;
+  op $backup_dir;
+}
+
+# this alias to open the current directory inside neovim
+alias nvm="nvm"
+
+# this function for nvm aliases
+# if we have one argument then nvim will be launched with it,
+# otherwise open the current directory if there is no argument
+function nvm {
+  nvim "${1:-.}";
+}
+
+# this alias to call neovim in a cooler way
+alias hnvim="nvim"
+
+# this alias to have the current view
+# of working directory content using ls
+alias cv="cv"
+
+# this function for cv alias
+# UPDATED : 01/25/2024
+# to adjust the title
+# when we have more than 50 visible items
+function cv {
+  local target="$1"
+  local folder_content="${target:-$PWD}"
+  local folder_name=$(basename $folder_content)
+  local visible_item=$(ls $folder_content | wc -l)
+  local total_item=$(ls -A $folder_content | wc -l)
+  local hidden_item=$((total_item - visible_item))
+
+  # this function for the header of cv alias
+  function show_header {
+    local folder_header
+    if [[ $total_item -eq 0 ]]; then
+      folder_header="Empty(0)";
+      folder_icon=" "
+    elif [[ $hidden_item -eq 0 ]]; then
+      folder_header="total($visible_item)";
+      folder_icon=" "
+    else
+      folder_header="visible($visible_item) hidden($hidden_item) total($total_item) ";
+      folder_icon=" "
+    fi
+    echo "${BOLD}${WHITE} $folder_icon $folder_name -> $folder_header ${RESET}";
+  }
+
+  # this function to show the content of the cv
+  function show_content {
+    if command -v eza >/dev/null 2>&1; then
+      eza --icons=always --no-quotes --group-directories-first $folder_content
+    else
+      ls $folder_content
+    fi
+  }
+
+  # this function to show the all of the cv content
+  function show_all {
+    c && br;
+
+    if [[ $visible_item -lt 30 ]]; then
+      show_header;
+      br;
+      show_content;
+      br;
+    else
+      show_content;
+      br 2;
+      show_header;
+      br;
+    fi
+  }
+  show_all;
+}
+
+# this alias to view the current directory content
+alias cvf="cvf"
+
+# this function for cvf alias
+# UPDATED : 01/25/2024
+# to adjust the title
+# when we have more than 50 visible items
+function cvf {
+  local target="$1"
+  local folder_content="${target:-$PWD}"
+  local folder_name=$(basename $folder_content)
+  local visible_item=$(ls $folder_content | wc -l)
+  local total_item=$(ls -A $folder_content | wc -l)
+  local hidden_item=$((total_item - visible_item))
+
+  # this function for the header of cv alias
+  function show_header {
+    local folder_header
+    if [[ $total_item -eq 0 ]]; then
+      folder_header="Empty(0)";
+      folder_icon=" "
+    elif [[ $hidden_item -eq 0 ]]; then
+      folder_header="total($visible_item)";
+      folder_icon=" "
+    else
+      folder_header="visible($visible_item) hidden($hidden_item) total($total_item) ";
+      folder_icon=" "
+    fi
+    echo "${BOLD}${WHITE} $folder_icon $folder_name -> $folder_header ${RESET}";
+  }
+
+  # this function to show the content of the cv
+  function show_content {
+    if command -v eza >/dev/null 2>&1; then
+      eza --icons=always --no-quotes -a --group-directories-first $folder_content;
+    else
+      ls -A $folder_content
+    fi
+  }
+
+  # this function to show the cv
+  function show_all {
+    c && br;
+
+    if [[ $hidden_item -lt 30 ]]; then
+      show_header;
+      br;
+      show_content;
+      br;
+    else
+      show_content;
+      br 2;
+      show_header;
+      br;
+    fi
+  }
+  show_all;
+}
+
+# this alias to view the current directory content
+# with specifications
+alias cvg="cvg"
+
+# this function for cvg alias
+function cvg {
+  local folder_name=$(basename $PWD)
+  local item="$1"
+  local matched_items=$(ls -A | grep "$item" | wc -l)
+
+  function show_header {
+    echo "${BOLD}   $folder_name -> contains $matched_items '$1' ${RESET}";
+  }
+
+  function show_content {
+    if command -v eza >/dev/null 2>&1; then
+      eza --icons=always --color=always -a --group-directories-first | grep "$1";
+    else
+      ls -A | grep "$1"
+    fi
+  }
+
+  # this function to show the cv
+  function show_all {
+    c && br;
+
+    if [[ $matched_items -lt 20 ]]; then
+      show_header $1; br;
+      show_content $1; br;
+    else
+      show_content $1; br;
+      show_header $1;
+    fi
+  }
+  show_all $item;
+}
+
+# this function to show tree
+# using eza if installed
+function tree {
+  if command -v eza >/dev/null 2>&1; then
+    eza -T --icons=always --no-quotes --group-directories-first "$@"
+  else
+    tree "$@"
+  fi
+}
+
+#######################################################################
+
+### p10k Config
+
+# To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
+[[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
+
+#######################################################################
